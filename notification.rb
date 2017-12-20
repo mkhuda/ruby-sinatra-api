70fbe35e9dceb914d1d1b444b35eb8b6b0452a56
mkhuda <mhmmd.huda@gmail.com>
2017-12-20 21:46:53 +0700

    use notification ruby

---
+++ notification.rb
@@ -0,0 +1,15 @@
+class Notification
+    def initialize
+        @receiver = ''
+        @isRead = false
+    end
+    
+    def validReceiver
+    end
+    
+    def markAsRead
+    end
+    
+    def status
+    end
+end
